00aefa923d017a585725a79db4691011f4ab61c9
Vivek Joshi <vivekjoshi41996@gmail.com>
2026-02-26 05:39:34 +0000

    Final Deploy: Standard ECS Service without CodeDeploy

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# --- 1. NETWORK DATA ---
+# --- 1. NETWORK ---
 data "aws_vpc" "default" { default = true }
 data "aws_subnets" "default" {
   filter {
@@ -13,17 +13,11 @@ data "aws_subnets" "default" {
 
 # --- 2. SECURITY GROUPS ---
 resource "aws_security_group" "alb_sg" {
-  name        = "strapi-alb-sg-v-final"
-  vpc_id      = data.aws_vpc.default.id
+  name   = "strapi-alb-sg-final-fix"
+  vpc_id = data.aws_vpc.default.id
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -36,8 +30,8 @@ resource "aws_security_group" "alb_sg" {
 }
 
 resource "aws_security_group" "ecs_sg" {
-  name        = "strapi-ecs-sg-v-final"
-  vpc_id      = data.aws_vpc.default.id
+  name   = "strapi-ecs-sg-final-fix"
+  vpc_id = data.aws_vpc.default.id
   ingress {
     from_port       = 1337
     to_port         = 1337
@@ -52,30 +46,24 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-# --- 3. LOAD BALANCER & TARGET GROUPS ---
+# --- 3. LOAD BALANCER ---
 resource "aws_lb" "main" {
-  name               = "strapi-alb-v-final"
+  name               = "strapi-alb-final-fix"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = data.aws_subnets.default.ids
 }
 
-resource "aws_lb_target_group" "blue" {
-  name        = "tg-blue-v-final"
+resource "aws_lb_target_group" "tg" {
+  name        = "tg-strapi-final-fix"
   port        = 1337
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = data.aws_vpc.default.id
-  health_check { path = "/" }
-}
-
-resource "aws_lb_target_group" "green" {
-  name        = "tg-green-v-final"
-  port        = 1337
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = data.aws_vpc.default.id
-  health_check { path = "/" }
+  health_check {
+    path = "/"
+    matcher = "200-399"
+  }
 }
 
 resource "aws_lb_listener" "http" {
@@ -84,46 +72,37 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
 
-# --- 4. ECS CLUSTER & TASK DEFINITION ---
+# --- 4. ECS CLUSTER & TASK ---
 resource "aws_ecs_cluster" "main" {
-  name = "strapi-cluster-v-final"
+  name = "strapi-cluster-final-fix"
 }
 
 resource "aws_ecs_task_definition" "strapi" {
-  family                   = "strapi-task-v-final"
+  family                   = "strapi-task-final-fix"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = "arn:aws:iam::811738710312:role/strapi-ecs-execution-role"
   task_role_arn            = "arn:aws:iam::811738710312:role/strapi-ecs-execution-role"
-
-  container_definitions = jsonencode([{
+  container_definitions    = jsonencode([{
     name  = "strapi-container"
     image = "strapi/strapi:latest"
-    portMappings = [{
-      containerPort = 1337
-      hostPort      = 1337
-      protocol      = "tcp"
-    }]
+    portMappings = [{ containerPort = 1337, hostPort = 1337, protocol = "tcp" }]
   }])
 }
 
 # --- 5. ECS SERVICE ---
 resource "aws_ecs_service" "strapi" {
-  name            = "strapi-service-v-final"
+  name            = "strapi-service-final-fix"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.strapi.arn
   launch_type     = "FARGATE"
   desired_count   = 1
-
-  deployment_controller {
-    type = "ECS"
-  }
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
@@ -132,63 +111,10 @@ resource "aws_ecs_service" "strapi" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.tg.arn
     container_name   = "strapi-container"
     container_port   = 1337
   }
-
-  lifecycle {
-    ignore_changes = [task_definition, load_balancer]
-  }
 }
 
-# --- 6. CODEDEPLOY APPLICATION & GROUP ---
-resource "aws_codedeploy_app" "strapi" {
-  compute_platform = "ECS"
-  name             = "StrapiApp-v-final"
-}
-
-resource "aws_codedeploy_deployment_group" "strapi" {
-  app_name               = aws_codedeploy_app.strapi.name
-  deployment_group_name  = "StrapiGroup-v-final"
-  service_role_arn       = "arn:aws:iam::811738710312:role/strapi-ecs-execution-role"
-  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
-
-  blue_green_deployment_config {
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-    }
-    terminate_blue_instances_on_deployment_success {
-      action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 1
-    }
-  }
-
-  deployment_style {
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-    deployment_type   = "BLUE_GREEN"
-  }
-
-  ecs_service {
-    cluster_name = aws_ecs_cluster.main.name
-    service_name = aws_ecs_service.strapi.name
-  }
-
-  load_balancer_info {
-    target_group_pair_info {
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.http.arn]
-      }
-      target_group {
-        name = aws_lb_target_group.blue.name
-      }
-      target_group {
-        name = aws_lb_target_group.green.name
-      }
-    }
-  }
-}
-
-output "ALB_DNS" {
-  value = aws_lb.main.dns_name
-}
+output "ALB_URL" { value = aws_lb.main.dns_name }
